f27db718d38208a2cfd90ed1747c5b10a3d31d72
Khant Alpesh <khantalpesh756@gmail.com>
2025-12-16 15:42:42 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,59 @@
+//*************** */1. C program to add two numbers****************//
+// var a: number = Number(prompt("Enter the a:"))
+// var b: number = Number(prompt("Enter the b:"))
+// var sum: number = a + b;
+// console.log("sum:" + sum);
+//  *******************2.call arithmetic*******************//
+// var a: number = Number(prompt("Enter the a:"))
+//  var b: number = Number(prompt("Enter the b:"))
+//  var sum: number = a + b;
+//  var sub: number = a - b;
+//  var mul: number = a * b;
+//  var div: number = a / b;
+//  console.log("sum:" + sum);
+//  console.log("sub:" + sub);
+//  console.log("mul:" + mul);
+//  console.log("div:" + div);
+//**********************3. C program to find perimeter of a rectangle
+//  var l: number = Number(prompt("Enter the l:"))
+//  var w: number = Number(prompt("Enter the w:"))
+//  var para:number=2 *(l + w);
+//  console.log("para is"+para);
+// *******************4.cC program to find area of a rectangle*********//
+// var l: number = Number(prompt("Enter the l:"))
+//  var w: number = Number(prompt("Enter the w:"))
+// var area:number= area =( l * w)  
+// console.log("area is",+area);
+// ******************************5.C program to find diameter, circumference and area of circle//
+// var r: number = Number(prompt("Enter the r:"))
+// var dia:number= 2 * r;
+// var cir:number= 2 * 3.14 * r;
+// var area:number=3.14 * (r* r);
+// console.log("dia is",+dia);
+// console.log("cir is",+cir);
+// console.log("area is",+area);
+// ********************6.cC program to convert centimeter to meter and kilometer
+// var cm: number = Number(prompt("Enter the cm"))
+// var meter:number = cm / 100.0;
+// var km :number   = cm / 100000;
+// console.log("meter is",+meter);
+// console.log("km is ",+km);
+// ****************7.cC program to convert temperature from degree celsius to fahrenheit****
+// var cel: number=Number(prompt("enter the cel"))
+//   var fahrenheit: number = (cel * 9 / 5) + 32;
+//   console.log("fahrenheit",+fahrenheit);
+// ***********************8.cC program to convert temperature from degree fahrenheit to celsius//
+// var fa:number=Number(prompt("enter the fa"))
+//  var celsius : number= (fa - 32) * 5 / 9;
+//  console.log("celsius",+celsius);
+// *******************9.c C program to find angles of triangle if two angles are given//
+// var a:number=Number(prompt("Enter the a"))
+// var b:number=Number(prompt("Enter the b"))
+// var c:number = 180 - (a + b);
+//  console.log("Third angle of the triangle "+c)
+// ************************10.c C program to calculate Simple Interest********//
+var p = Number(prompt("Enter the p"));
+var t = Number(prompt("Enter the t"));
+var r = Number(prompt("Enter the r"));
+var SI = (p * t * r) / 100;
+console.log("Simple Interest =  SI", +SI);
